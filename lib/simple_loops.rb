33348bd2cb4776b29a007e93e98e076cea0d719a
simon cohen <escohen115@gmail.com>
2020-10-12 19:12:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ def return_string_array(array)
   counter = 0
   new_array=[]
   while counter < array.length
-    new_array.push(array[counter])
+    new_array.push(array[counter].to_s)
     counter += 1
   end
   new_array
